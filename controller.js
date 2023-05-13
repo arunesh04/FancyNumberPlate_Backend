a1060799c9d01ebea282043c6979e77b76a351fb
arunesh04 <aruneshj.21aid@kongu.edu>
2023-05-13 15:20:31 +0530

    Crud operations

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 const { NumberPlate, User, Admin } = require('./schema');
+//Inserting Operation
 module.exports.insertUser = async (req,res) => {
     const user = new User({
         name: req.body.name,
@@ -34,4 +35,47 @@ module.exports.insertNumplate = async (req,res) => {
     })
     await numberplate.save();
     res.send({msg:"Number Added Sucessfully !"});
+}
+
+//Get Operation 
+
+module.exports.getAdmin = async (req,res) => {
+    const admin = await Admin.find({});
+    res.send(admin)
+}
+
+module.exports.getUser = async (req,res) => {
+    const user = await User.find({});
+    res.send(user)
+}
+
+module.exports.getNumberplate = async (req,res) => {
+    const numplate = await NumberPlate.find({});
+    res.send(numplate)
+}
+
+//Delete operation 
+
+module.exports.deleteAdmin = async (req,res) => {
+    const admin = await Admin.findOneAndDelete({adminid:req.params.adid});
+    if(admin)
+        res.send("Admin Deleted Sucessfully!");
+    else
+        res.send("Admin Doesn't Exist!");
+}
+
+module.exports.deleteUser = async (req,res) => {
+    const user = await User.findOneAndDelete({email:req.params.email});
+    if(user)
+        res.send("User Deleted Sucessfully!");
+    else
+        res.send("User Doesn't Exist!");
+}
+
+module.exports.deleteNumberplate = async (req,res) => {
+    const numberplate = await NumberPlate.findOneAndDelete({plateid:req.params.plateId});
+    if(numberplate)
+        res.send("Numberplate Deleted Sucessfully!");
+    else
+        res.send("Numberplate Doesn't Exist!");
 }
